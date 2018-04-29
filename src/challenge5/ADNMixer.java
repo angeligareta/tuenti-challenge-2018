d53bcfb4bad3e7c298ee0232a3786acc6c0bfc2f
Ángel Igareta <alu0100967111@ull.edu.es>
2018-04-29 15:40:35 -0700

    Challenge 5 finished.

--- src/challenge5/ADNMixer.java
+++ src/challenge5/ADNMixer.java
@@ -7,24 +7,25 @@
  * 'gTTC'. However, to make it interesting, they come mixed with parts from
  * another experiment such as 'aaaa' 'cccc' and 'gttc'.
  * 
- * In order to find the original sequence, we have to make permutations of
- * strings of a size. For example, we start with strings formed by two strings
- * and, if during the permutation, we see that this string has been already
- * formed and the words used to form it are different than the ones used to form
- * the current one, we found the solution.
+ * In order to find the original sequence, first of all we make an array with the 
+ * possible solutions, those are the ones that start with the same set of characters. 
+ * After that, we try calling the addSubstring method with each pair of
+ * solutions until we find two solution with the same string.
  * 
- * If during that process we don't find a pair of concatenated strings equal, we
- * make the same process but choosing the strings formed by 3 strings. We repeat
- * the process until a solution is found or the size of the subgroups is higher
- * than the half of the number of strings.
+ * The addSubstring method receives a pair of solutions, look for the difference 
+ * between the pair ADN string and look for a non-visited ADN part that contains the
+ * difference or a part of it. Once the part is found, we call this method
+ * recursivelY in case that string is not the one that is going to help finding
+ * the final substring.
  * 
  * Finally, to receive the test we have to connect to an ip and solve the tests
  * in a fixed time. Let's do it!
- *
+ * 
  * @author Ángel Igareta (angel@igareta.com)
  * @version 1.0
  * @since 28-04-2018
- */
+*/
+
 package challenge5;
 
 import java.io.BufferedReader;
@@ -43,10 +44,6 @@ import java.util.TreeSet;
  * solutions.
  */
 public class ADNMixer {
-
-	/** Sorted set of integer that indicate the final solution indexes. */
-	private static TreeSet<Integer> finalSolution = new TreeSet<Integer>();
-
 	/** Parameters for socket. */
 	private static final int PORT = 3241;
 	private static final String IP = "52.49.91.111";
@@ -56,172 +53,162 @@ public class ADNMixer {
 	private static final String OUTPUT_FILE = "src/challenge5/key";
 
 	/**
-	 * Main method.
+	 * Main method. Comunicate with the socket and call getSolution method.
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println(getSolutionOf(
-				"TATaCaACcCG aAgGacctcTtGgt TAttaCAtaTtA CgG TAttaCAtaTtAC CTATaCaACcCGcGg CCcCTaGcATaC cTgTGGAGAg cGgCgG"));
-		// try {
-		// Socket socket = new Socket(IP, PORT);
-		// BufferedReader reader = new BufferedReader(new
-		// InputStreamReader(socket.getInputStream()));
-		// PrintWriter writer = new PrintWriter(socket.getOutputStream());
-		// System.out.println("Connection stablished with: " + socket.getInetAddress());
-		// System.out.println("Starting Challenge 5!");
-		//
-		// // Getting the number of Cases
-		// String firstLine = reader.readLine();
-		// int numberOfCases = Integer.parseInt(firstLine.split("\\s")[1]);
-		//
-		// // Choosing mode
-		// System.out.println(reader.readLine());
-		// System.out.println("Setting MODE: " + MODE);
-		// ;
-		// writer.println(MODE);
-		// writer.flush();
-		//
-		// // Starting tests
-		// String key = "";
-		// System.out.println(reader.readLine());
-		// for (int i = 0; i < numberOfCases; ++i) {
-		// String ADN = reader.readLine();
-		// System.out.println(ADN);
-		// String solution = getSolutionOf(ADN);
-		// System.out.println(solution);
-		//
-		// writer.println(solution);
-		// writer.flush();
-		// key = reader.readLine();
-		// System.out.println(key);
-		// }
-		//
-		// reader.close();
-		// writer.close();
-		// socket.close();
-		//
-		// BufferedWriter keyWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE));
-		// keyWriter.write(key.split("\"")[1]); // To write the key
-		// keyWriter.close();
-		// }
-		// catch (IOException e) {
-		// e.printStackTrace();
-		// }
-	}
+		try {
+			Socket socket = new Socket(IP, PORT);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+			System.out.println("Connection stablished with: " + socket.getInetAddress());
+			System.out.println("Starting Challenge 5!");
 
-	public static String getSolutionOf(String ADN) {
-		finalSolution = new TreeSet<Integer>();
-		ArrayList<String> aDNParts = new ArrayList<String>(Arrays.asList(ADN.split("\\s")));
-		int maxSizeOfSubGroups = aDNParts.size() / 2;
+			// Getting the number of Cases
+			String firstLine = reader.readLine();
+			int numberOfCases = Integer.parseInt(firstLine.split("\\s")[1]);
 
-		// Array to initialize the base solution, formed simply by the ADNParts without
-		// permute.
-		ArrayList<Solution> lastPermutedSolution = new ArrayList<Solution>();
-		for (int i = 0; i < aDNParts.size(); ++i) {
-			ArrayList<Integer> indexArray = new ArrayList<Integer>();
-			indexArray.add(i);
-			lastPermutedSolution.add(new Solution(aDNParts.get(i), indexArray));
-		}
-		ArrayList<Solution> initialPermutedSolution = new ArrayList<Solution>(lastPermutedSolution);
-		ArrayList<Integer> bannedSolution = new ArrayList<Integer>();
+			// Choosing mode
+			System.out.println(reader.readLine());
+			System.out.println("Setting MODE: " + MODE);
+			;
+			writer.println(MODE);
+			writer.flush();
 
-		// Loop to permute between loops of size i + 1
-		for (int i = 1; i < maxSizeOfSubGroups; ++i) {
-			ArrayList<Solution> currentPermutatedSolution = permutateGroupsOf(initialPermutedSolution, lastPermutedSolution);
-			if (currentPermutatedSolution == null) { // Solution Found
-				break;
+			// Starting tests
+			String key = "";
+			System.out.println(reader.readLine());
+			for (int i = 0; i < numberOfCases; ++i) {
+				String ADN = reader.readLine();
+				System.out.println(ADN);
+				String solution = getSolutionOf(ADN);
+				System.out.println(solution);
+
+				writer.println(solution);
+				writer.flush();
+				key = reader.readLine();
+				System.out.println(key);
 			}
-			else {
-				// bannedSolution = getBannedSolution(currentPermutatedSolution);
-				lastPermutedSolution = currentPermutatedSolution;
-			}
-		}
 
-		if (finalSolution == null) {
-			throw new IllegalArgumentException("Not solution Found");
+			reader.close();
+			writer.close();
+			socket.close();
+
+			BufferedWriter keyWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE));
+			keyWriter.write(key.split("\"")[1]); // To write the key
+			keyWriter.close();
 		}
-		else {
-			String result = "";
-			for (Integer finalIndex : finalSolution) {
-				result += (finalIndex + 1) + ",";
-			}
-			return result.substring(0, result.length() - 1);
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * @param currentPermutatedSolution
+	 * Method that receives an ADN String Array and found the parts of the original
+	 * ADN String. First of all we make an array with the possible solutions, those
+	 * are the ones that start with the same set of characters.
+	 * 
+	 * After that, we try calling the addSubstring method with each pair of
+	 * solutions until we find two solution with the same string.
 	 */
-	private static ArrayList<Integer> getBannedSolution(ArrayList<Solution> currentPermutatedSolution) {
-		ArrayList<Integer> bannedSolutions = new ArrayList<Integer>();
+	public static String getSolutionOf(String ADN) {
+		final ArrayList<String> aDNParts = new ArrayList<String>(Arrays.asList(ADN.split("\\s")));
 
-		for (int i = 0; i < currentPermutatedSolution.size(); ++i) {
-			boolean contained = false;
-			String currentString = currentPermutatedSolution.get(i).getConcatenedString();
-			for (int j = 0; j < currentPermutatedSolution.size(); ++j) {
+		ArrayList<Solution> solutionArray = new ArrayList<Solution>();
+		for (int i = 0; i < aDNParts.size(); ++i) {
+			for (int j = 0; j < aDNParts.size(); ++j) {
 				if (i == j) {
 					continue;
 				}
-				String secondaryString = currentPermutatedSolution.get(j).getConcatenedString();
-				if (currentString.contains(secondaryString.substring(secondaryString.length() / 2))
-						|| currentString.contains(secondaryString.substring(0, secondaryString.length() / 2))) {
-					System.out.println("Containing: " + currentString + " " + secondaryString);
-					contained = true;
-				}
-			}
-			if (!contained) {
-				// System.out.println("Removing " + currentPermutatedSolution.get(i));
-				for (Integer bannedIndex : currentPermutatedSolution.get(i).getIndexUsedForConcat()) {
-					if (!bannedSolutions.contains(bannedIndex)) {
-						bannedSolutions.add(bannedIndex);
-					}
+				else if (aDNParts.get(i).startsWith(aDNParts.get(j))) {
+					solutionArray.add(new Solution(aDNParts.get(i), new ArrayList<Integer>(Arrays.asList(i))));
+					solutionArray.add(new Solution(aDNParts.get(j), new ArrayList<Integer>(Arrays.asList(j))));
 				}
 			}
 		}
 
-		System.out.println("\nBanned: ");
-		for (Integer i : bannedSolutions) {
-			System.out.print(i + " ");
+		boolean finished = false;
+		while (!finished) {
+			// Try pair of solutions
+			ArrayList<Solution> currentSolution = new ArrayList<Solution>(solutionArray.subList(0, 2));
+			finished = addSubString(currentSolution, aDNParts);
+
+			if (finished) {
+				solutionArray.clear();
+				solutionArray.addAll(currentSolution);
+			}
+			else if (solutionArray.size() > 1) { // If there are more initial solutions, try with them
+				solutionArray.remove(0);
+				solutionArray.remove(0);
+			}
+			else {
+				throw new IllegalArgumentException("There is no solution!");
+			}
 		}
 
-		return bannedSolutions;
+		TreeSet<Integer> solutionIndexesArray = new TreeSet<Integer>();
+		solutionIndexesArray.addAll(solutionArray.get(0).getIndexUsedForConcat());
+		solutionIndexesArray.addAll(solutionArray.get(1).getIndexUsedForConcat());
+
+		String result = "";
+		for (Integer index : solutionIndexesArray) {
+			result += (index + 1) + ",";
+		}
+		return result.substring(0, result.length() - 1);
 	}
 
 	/**
-	 * Method that permutes the ADNParts of size 1 with the base Array received by
-	 * argument. If during that process there is another solution with the same
-	 * string and formed by different indexes, it adds the two solutions to the
-	 * finalSolution object.
+	 * Method that receives a pair of solutions, look for the difference between the
+	 * pair ADN string and look for a non-visited ADN part that contains the
+	 * difference or a part of it. Once the part is found, we call this method
+	 * recursively in case that string is not the one that is going to help finding
+	 * the final substring.
 	 */
-	private static ArrayList<Solution> permutateGroupsOf(ArrayList<Solution> initialPermutedSolution,
-			ArrayList<Solution> baseArray) {
-		ArrayList<Solution> concatStringMap = new ArrayList<Solution>();
-		for (int i = 0; i < baseArray.size(); ++i) {
-			Solution currentBaseSolution = baseArray.get(i);
+	private static boolean addSubString(ArrayList<Solution> currentSolutionArray, ArrayList<String> aDNParts) {
+		if (currentSolutionArray.get(0).equals(currentSolutionArray.get(1))) {
+			return true;
+		}
 
-			for (int j = 0; j < initialPermutedSolution.size(); ++j) {
-				if (currentBaseSolution.getIndexUsedForConcat().contains(j)) {
-					continue;
-				}
-				ArrayList<Integer> newIndexArray = currentBaseSolution.getIndexUsedForConcat();
-				newIndexArray.add(j);
-				String currentConcatenedString = currentBaseSolution.getConcatenedString()
-						.concat(initialPermutedSolution.get(j).getConcatenedString());
-				System.out.println(
-						currentBaseSolution.getConcatenedString() + " + " + initialPermutedSolution.get(j).getConcatenedString());
+		// Get minimum length solution index
+		String firstString = currentSolutionArray.get(0).getConcatenedString();
+		String secondString = currentSolutionArray.get(1).getConcatenedString();
+		int minimumSolutionIndex = (firstString.length() < secondString.length()) ? 0 : 1;
 
-				Solution newSolution = new Solution(currentConcatenedString, newIndexArray);
-				if (concatStringMap.contains(newSolution)) {
-					finalSolution.addAll(concatStringMap.get(concatStringMap.indexOf(newSolution)).getIndexUsedForConcat());
-					finalSolution.addAll(newSolution.getIndexUsedForConcat());
-					return null;
-				}
-				else {
-					concatStringMap.add(newSolution);
+		String differenceString = (minimumSolutionIndex == 0) ? secondString.substring(firstString.length())
+				: firstString.substring(secondString.length());
+
+		// Look for difference string
+		for (int i = 0; i < aDNParts.size(); ++i) {
+			ArrayList<Integer> visitedIndexes = currentSolutionArray.get(0).getIndexUsedForConcat();
+			visitedIndexes.addAll(currentSolutionArray.get(1).getIndexUsedForConcat());
+
+			// If there is a string that starts with the difference or contains a part of
+			// it, try looking for the solution recursively with that string.
+			if (!visitedIndexes.contains(i)
+					&& (aDNParts.get(i).startsWith(differenceString) || differenceString.startsWith(aDNParts.get(i)))) {
+				ArrayList<Solution> newSolutionArray = new ArrayList<Solution>(currentSolutionArray);
+
+				// Generate new solution attributes.
+				ArrayList<Integer> newSolutionIndexesArray = currentSolutionArray.get(minimumSolutionIndex)
+						.getIndexUsedForConcat();
+				String newConcatenedString = currentSolutionArray.get(minimumSolutionIndex).getConcatenedString();
+				newSolutionIndexesArray.add(i);
+				newConcatenedString = newConcatenedString.concat(aDNParts.get(i));
+
+				// Swap the current element with the new solution
+				newSolutionArray.remove(minimumSolutionIndex);
+				newSolutionArray.add(new Solution(newConcatenedString, newSolutionIndexesArray));
+
+				// Recursive solve it
+				if (addSubString(newSolutionArray, aDNParts)) {
+					currentSolutionArray.clear();
+					currentSolutionArray.addAll(newSolutionArray);
+					return true;
 				}
 			}
 		}
 
-		return concatStringMap;
+		// No substring to continue found
+		return false;
 	}
 
 	/**
